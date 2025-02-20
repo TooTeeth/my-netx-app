1fe1877651cb84c6de1c5e3262461b35db21b6b9
dinnerjin <sio015@naver.com>
2025-02-20 16:15:41 +0900

    first-commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-4">
+      <p className="text-lg text-gray-600">404 Not Found</p>
+    </main>
+  );
+}
